4c5e67f2c31341605cdc11a7d1880c22b76c6362
ZheChen-PC\ZheChen <chenzhezju@gmail.com>
2017-09-20 00:00:04 -0700

    Added note to code m_8, which corrects the exy of Vic2D output

--- m_8_corrected_exy_in_DIC.m
+++ m_8_corrected_exy_in_DIC.m
@@ -1,7 +1,10 @@
-
 % chenzhe, 2017-06-08.
 % correct the exy field of DIC data, i.e., flip the sign.
-% add a note exy_corrected = 1.
+% If corrected, append a varialbe exy_corrected = 1.
+%
+% chenzhe, 2017-09-19 note
+% currently only corrects 'exy' variable.  Can modify if you've processed
+% and renamed the data, but it's better to keep 'exy' as default.
 
 [f,p] = uigetfile('D:\Marissa_test_20170430_stitched_DIC\xyuv_dic_NonIncremental','select dic files need to be rotated','multiselect','on');
 if ~iscell(f)
